726de77282108ca370f21f7c437895a40f659ead
StoicTylerDurden <buhussayyen@gmail.com>
2024-08-14 11:42:01 +0300

    Auth template is done

---
+++ server.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config();
+
+require('./config/database');
+const express = require('express');
+
+// Auth
+const verifyToken = require('./middleware/verify-token');
+
+// Controllers
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.use('/test-jwt', testJWTRouter);
+app.use('/users', usersRouter);
+app.use('/profiles', verifyToken, profilesRouter);
+
+app.listen(PORT, () => {
+  console.log('The express app is ready!');
+});
